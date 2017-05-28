d22a2bd8a347d181cf0e47807c6d12cc8fcf525c
Carlos <leal.lozano@gmail.com>
2017-05-28 18:14:23 -0400

    Juego 3 en raya Culminada 1 player

--- app/src/main/java/com/example/carlos/ejercicio3enraya/MainActivity.java
+++ app/src/main/java/com/example/carlos/ejercicio3enraya/MainActivity.java
@@ -1,13 +1,176 @@
 package com.example.carlos.ejercicio3enraya;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Iniciamos el array casillas que identifica cada casilla y la almacena en el array
+
+        CASILLAS = new int[9];
+        CASILLAS[0]=R.id.a1;
+        CASILLAS[1]=R.id.a2;
+        CASILLAS[2]=R.id.a3;
+        CASILLAS[3]=R.id.b1;
+        CASILLAS[4]=R.id.b2;
+        CASILLAS[5]=R.id.b3;
+        CASILLAS[6]=R.id.c1;
+        CASILLAS[7]=R.id.c2;
+        CASILLAS[8]=R.id.c3;
     }
+
+    public void aJugar(View vista) {
+
+        ImageView imagen;
+
+        for(int cadaCasilla:CASILLAS) {
+            imagen=(ImageView)findViewById((cadaCasilla));
+            imagen.setImageResource(R.drawable.casilla);
+        }
+
+        jugadores=1;
+        if(vista.getId()==R.id.dosjug) {
+            jugadores=2;
+        }
+
+        RadioGroup configDificultad=(RadioGroup)findViewById(R.id.configD);
+
+        int id=configDificultad.getCheckedRadioButtonId();
+
+        int dificultad=0;
+
+        if (id==R.id.normal) {
+            dificultad=1;
+        }else if (id==R.id.imposible){
+            dificultad=2;
+        }
+
+
+        partida = new Partida (dificultad);
+
+
+
+
+        ((Button)findViewById(R.id.unjug)).setEnabled(false);
+
+        ((RadioGroup)findViewById(R.id.configD)).setAlpha(0);
+
+        ((Button)findViewById(R.id.dosjug)).setEnabled(false);
+
+    }
+
+    public void  toque(View miVista) {
+
+        if (partida == null){
+            return;
+        }
+
+        int casilla=0;
+
+        for (int i=0; i<9;i++) {
+
+            if (CASILLAS[i]== miVista.getId()) {
+
+                casilla=i;
+                break;
+            }
+        }
+
+        /*Toast toast = Toast.makeText(this,"Has pulsasdo la casilla "+ casilla,Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.show();*/
+
+        if(partida.comprueba_casilla(casilla)==false){
+
+            return;
+        }
+
+        marca(casilla);
+
+        int resultado=partida.turno();
+
+        if(resultado>0) {
+
+            termina(resultado);
+            return;
+
+        }
+
+        casilla=partida.ia();
+
+        while(partida.comprueba_casilla(casilla)!=true) {
+
+            casilla=partida.ia();
+
+        }
+
+        marca(casilla);
+
+        resultado=partida.turno();
+
+        if(resultado>0) {
+
+            termina(resultado);
+
+        }
+
+    }
+
+
+    private void termina(int resultado) {
+
+        String mensaje;
+
+        if(resultado==1) mensaje=getString(R.string.gana);//"Gana el jugador 1";
+
+        else if(resultado==2) mensaje=getString(R.string.pierde);//"Gana el jugador 2";
+
+        else mensaje=getString(R.string.empate);//"Empate";
+
+        Toast toast = Toast.makeText(this,mensaje,Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.show();
+
+        partida=null;
+
+        ((Button)findViewById(R.id.unjug)).setEnabled(true);
+        ((RadioGroup)findViewById(R.id.configD)).setAlpha(1);
+        ((Button)findViewById(R.id.dosjug)).setEnabled(true);
+
+    }
+
+
+    private void marca (int casilla) {
+
+        ImageView imagen;
+        imagen = (ImageView)findViewById(CASILLAS[casilla]);
+
+        if (partida.jugador==1) {
+            imagen.setImageResource(R.drawable.circulo);
+        } else {
+
+            imagen.setImageResource(R.drawable.aspa);
+        }
+
+    }
+
+
+    private int jugadores;
+    private int [] CASILLAS;
+    private Partida partida;
+
+
+
 }
---
+++ app/src/main/java/com/example/carlos/ejercicio3enraya/Partida.java
@@ -0,0 +1,141 @@
+package com.example.carlos.ejercicio3enraya;
+
+import java.util.Random;
+
+/**
+ * Created by Carlos on 5/28/2017.
+ */
+
+public class Partida {
+
+    public Partida (int dificultad) {
+        this.dificultad=dificultad;
+
+        jugador=1;
+
+        casillas = new int [9];
+        for(int i=1;i<9;i++) {
+            casillas[i]=0;
+        }
+    }
+
+    public boolean comprueba_casilla (int casilla) {
+        if (casillas[casilla]!=0) {
+            return false;
+        }else {
+            casillas[casilla]=jugador;
+        }
+        return true;
+    }
+
+
+    public int turno() {
+
+        boolean empate=true;
+
+        boolean ult_movimiento=true;
+
+        for (int i=0;i<COMBINACIONES.length;i++){
+            for(int pos:COMBINACIONES[i]) {
+
+               // System.out.println("Valor en posición " + pos + " " + casillas[pos]);
+
+                if (casillas[pos]!=jugador)ult_movimiento=false;
+
+                if(casillas[pos]==0) {
+                    empate=false;
+                }
+
+            } //cierre for anidado
+
+            //System.out.println("--------------------------------------------------------");
+
+            if (ult_movimiento)return jugador;
+            ult_movimiento=true;
+
+        } //cierre for principal
+
+        if (empate) {
+            return 3;
+        }
+
+
+        jugador++;
+        if (jugador >2) {
+            jugador=1;
+        }
+        return 0;
+    }
+
+
+    public int dosEnRaya(int jugador_en_turno){ //Video 53
+
+        int casilla=-1;
+
+        int cuantas_lleva=0;
+
+        for (int i=0;i<COMBINACIONES.length;i++){
+
+            for(int pos:COMBINACIONES[i]) {
+
+                if (casillas[pos]==jugador_en_turno) cuantas_lleva++;
+
+                if (casillas[pos]==0) casilla=pos;
+
+            } //cierre for anidado
+
+            if(cuantas_lleva==2 && casilla!=-1) return casilla;
+
+            casilla=-1;
+            cuantas_lleva=0;
+
+        } //cierre for principal
+
+        return -1;
+
+    }
+
+
+    public int ia () {
+
+        int casilla;
+
+        casilla=dosEnRaya(2);
+
+        if(casilla!=-1) return casilla;
+
+        if (dificultad>0) {
+
+            casilla=dosEnRaya(1);
+
+            if (casilla!=-1) return casilla;
+
+        }
+
+        if(dificultad==2) {
+
+            if(casillas[0]==0) return 0;
+            if(casillas[2]==0) return 2;
+            if(casillas[6]==0) return 6;
+            if(casillas[8]==0) return 8;
+
+        }
+
+        Random casilla_azar = new Random();
+
+        casilla=casilla_azar.nextInt(9);
+
+        return casilla;
+
+    }
+
+
+
+    public final int dificultad;
+    public int jugador;
+    private int [] casillas;
+    private final int [] [] COMBINACIONES={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+
+
+
+}
